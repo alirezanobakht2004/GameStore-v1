e8c38ddf9f50ca698e6e6742e1ab74f8b741c569
alirezanobakht2004 <alireza.nobakht2004@gmail.com>
2023-05-05 22:45:26 +0430

    final version

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -1,19 +1,10 @@
 package ir.ac.kntu;
 
-import java.util.ArrayList;
-
 public class Main {
-
-    private ArrayList<Game> gamesArr = new ArrayList<Game>();
-
-    public ArrayList<Game> getGamesArr() {
-        return gamesArr;
-    }
 
     public static void main(String[] args) {
         Sign start = new Sign();
         start.sign();
-
     }
 
 }
